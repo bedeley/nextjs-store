6111852dc7d48fdd3f4de49ce6addd089ad12b59
bedeley <bedeley@gmail.com>
2025-05-14 18:26:10 -0400

    change clerk key

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,7 @@ export default clerkMiddleware((auth, req) => {
   const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
 
   if (isAdminRoute(req) && !isAdminUser) {
-        return NextResponse.redirect(new URL('/', req.url))
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   if (!isPublicRoute(req)) auth().protect();
